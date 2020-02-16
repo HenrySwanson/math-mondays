a6b68793fe7dcef1746c312d8c8268655208409f
Henry Swanson <henryswanson94@gmail.com>
2020-02-16 15:49:44 -0800

    Alright! Got mathjax rendering working
    Had to do some reading on exs and ems, but I think it's all correct
    now. Like with standard text, it renders with the baseline at 0.
    
    Also, I fixed some miscomputation in the size of regular text. Still
    a little confused on what exactly a font size _is_, but I know that
    for what I have right now, fontSizePx = 16 means an x is 8px tall. So
    I think that's right, even though capital letters aren't a full 16px.

--- _asset_creation/hydra.js
+++ _asset_creation/hydra.js
@@ -1,8 +1,46 @@
 "use strict";
 
-/* Nonsense for setting up fonts and canvases. */
-// Right now we're using svgdom to create the SVG, and node-canvas to render it
-// to PNG. Hopefully we can just use the latter.
+//------------------------------------------
+// Nonsense for setting up fonts and canvases
+//-------------------------------------------
+
+// Set up a fake DOM with a single SVG element at the root
+const window = require('svgdom');
+const SVG = require('svg.js')(window)
+
+//--------------------------------
+// Nonsense for setting up MathJax
+//--------------------------------
+
+const PACKAGES = 'base, autoload, require, ams, newcommand';
+global.MathJax = {
+    tex: {packages: PACKAGES.split(/\s*,\s*/)},
+    svg: {fontCache: 'local'},
+    startup: {typeset: false}
+};
+
+require('mathjax/es5/startup.js');
+require('mathjax/es5/core.js');
+require('mathjax/es5/adaptors/liteDOM.js');
+require('mathjax/es5/input/tex-base.js');
+require('mathjax/es5/input/tex/extensions/all-packages.js');
+require('mathjax/es5/output/svg.js');
+require('mathjax/es5/output/svg/fonts/tex.js');
+
+MathJax.loader.preLoad(
+    'core',
+    'adaptors/liteDOM',
+    'input/tex-base',
+    '[tex]/all-packages',
+    'output/svg',
+    'output/svg/fonts/tex'
+);
+
+MathJax.config.startup.ready();
+
+//------------------------------
+// Now we can write our own code
+//------------------------------
 
 const fs = require('fs');
 const TextToSVG = require('text-to-svg');
@@ -10,11 +48,6 @@ const util = require('util');
 
 const hydralib = require("../js/hydra_lib.js");
 
-// Set up a fake DOM with a single SVG element at the root
-const window = require('svgdom');
-const SVG = require('svg.js')(window)
-
-// Load fonts
 const ALTE_DIN = TextToSVG.loadSync("fonts/alte-din-1451-mittelschrift/din1451alt.ttf");
 
 function shrinkCanvas(canvas) {
@@ -44,14 +77,71 @@ function shrinkCanvas(canvas) {
 	canvas.viewbox(box.x, box.y, box.w, box.h);
 }
 
-function makeTextPath(canvas, textToSvgObj, text, nfontSize) {
-	var originalSize = 72;  // big enough
-	var pathData = textToSvgObj.getD(text, {fontSize: originalSize});
-	var path = canvas.path(pathData);
-	// font size != SVG size, but should be proportional, so we scale down from
-	// current size
-	path.size(path.width() * nfontSize / originalSize, null); 
+function scaleFromOrigin(svgObj, scaleFactor) {
+	// Oftentimes, mostly for text, we want to scale from the origin, not from
+	// the upper-left corner. So we do it here.
+	var oldBBox = svgObj.bbox();
+	svgObj.size(oldBBox.width * scaleFactor);
+
+	// We know where the upper-left corner of the bbox should be, because
+	// scaling from the origin is easy. So let's move the object there.
+	svgObj.move(oldBBox.x * scaleFactor, oldBBox.y * scaleFactor);
+}
+
+function makeTextPath(canvas, textToSvgObj, text, fontSizePx) {
+	// 72 is big enough to render crisply
+	var path = canvas.path(textToSvgObj.getD(text, {fontSize: 72}));
+
+	// The text is created with the start of the baseline at the origin, so
+	// we use scaleFromOrigin, and not size().
+	var scaleFactor = fontSizePx / 72;
+	scaleFromOrigin(path, scaleFactor);
+
+	// TODO there's still some weird leading space at the beginning?
+
 	return path;
+}
+
+function makeMathSvg(canvas, tex, fontSizePx) {
+	// Returns some math text with the baseline at the origin.
+	const adaptor = MathJax.startup.adaptor;
+	const node = MathJax.tex2svg(tex, {display: true});
+	var svgText = adaptor.outerHTML(node.children[0]);
+	canvas.svg(svgText);  // add inner SVG element to the canvas
+
+	// get the last element, which is our svg object
+	var children = canvas.children();
+	var mathSvg = children[children.length-1];
+
+	// Unfortunately, MathJax outputs units of ex, but that's not compatible
+	// with svg.js. Fortunately, we can ask it for its context.
+	var metrics = MathJax.getMetricsFor(node, true);
+	var exToPx = metrics.ex / metrics.scale;
+
+	// Now we change the units to px (this should not change the visual size).
+	var heightEx = +mathSvg.height().slice(0,-2);
+	mathSvg.size(null, heightEx * exToPx);
+
+	// Next, we shift it so that the baseline is at zero. To do this, we have to
+	// inspect the "style" property of the object; specifically, the property
+	// vertical-align. Fortunately, that should be the only thing in there.
+	var regex = /vertical-align:(-?\d*\.?\d*)(ex)?/
+	var verticalAlignEx = +regex.exec(mathSvg.attr("style"))[1];
+
+	// Currently the upper-left corner is at the origin, so we want to raise it
+	// by the height, and then by vertical-align. But this all has to be done
+	// in units of px, not ex.
+	mathSvg.dy(-exToPx * (heightEx + verticalAlignEx));
+
+	// Now the baseline is at the origin, so let's scale from the origin. The font
+	// is, by default, 1 em tall, so that's what we put in our denominator.
+	var scaleFactor = fontSizePx / metrics.em * metrics.scale;
+	scaleFromOrigin(mathSvg, scaleFactor);
+	
+	// Lastly, clear the style element so it can't interfere with us
+	mathSvg.attr("style", "");
+
+	return mathSvg;
 }
 
 function saveImgToFile(canvas, filename, highlightBackground) {
@@ -65,6 +155,7 @@ function saveImgToFile(canvas, filename, highlightBackground) {
 
 	fs.writeFileSync(filename, canvas.svg());
 }
+
 
 // Start creating some diagrams!
 // This is the actual canvas we want to work on
@@ -105,7 +196,7 @@ var marker = canvas.marker(5, 5, function(add) {
   add.polygon("0,0 5,2.5 0,5").fill(textColor);
 });
 
-canvas.path("M -2.3,0.9 Q -2.3,2 -0.6,2").stroke(stroke).fill('none').marker('end', marker);
+canvas.path("M -2.3,0.8 Q -2.3,2 -0.6,2").stroke(stroke).fill('none').marker('end', marker);
 
 function makeCubicPath(startHeight, endHeight, control1, control2) {
 	return util.format(
@@ -326,3 +417,39 @@ var mouth = groupC.path("M -1,1 A 1.2,1.2 0 0,1 1,1").size(0.27)
 
 // don't shrink canvas, so we have the same size as before
 saveImgToFile(canvas, "assets/hydra/example-5.svg");
+
+
+//------------------------------------------------
+// Diagram: Ordinal sequence for killing the hydra
+//------------------------------------------------
+
+canvas.clear();
+
+var values = [
+	"\\omega^2", "3 \\omega", "2 \\omega + 3", "2 \\omega",
+	"\\omega + 3", "\\omega", "3", "0",
+];
+for (var i=0; i < 8; i++) {
+	// Where's the UL corner of our cell?
+	var cellX = (i % 4) * 8;
+	var cellY = (i < 4) ? 0 : 8;
+
+	// Make the hydra
+	var h = makeHydra(hydraStrs[i]);
+	hydralib.computeHydraLayout(h);
+	hydralib.drawHydraImmediately(h);
+
+	// Group the hydra so you can move it.
+	var grp = groupHydra(h);
+	grp.dy(-h.svgHead.y())  // put the root at y=0
+	grp.dmove(cellX, cellY);  // put into the proper cell
+
+	var value = makeMathSvg(canvas, values[i], 1);
+	// put the center of the bottom at the origin
+	value.dx(-value.width() / 2);
+	value.dmove(cellX + 2, cellY + 4);
+}
+// TODO some bug in shrinkCanvas when math is present, gotta do it manually
+canvas.viewbox(-0.5, -2, 3 * 8 + 4 + 1, 14);
+
+saveImgToFile(canvas, "assets/hydra/ordinals.svg");
