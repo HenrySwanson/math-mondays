01746b273442608d98045593e5574ef3b58c173e
Henry Swanson <henryswanson94@gmail.com>
2022-01-07 23:48:05 -0500

    Add a separate command for generating files than for testing
    Also added a real test.

--- asset-generator/test/index.spec.ts
+++ asset-generator/test/index.spec.ts
@@ -1,5 +1,16 @@
-import { hello } from "../src";
+import { Tree } from "../src/lib/tree"
+import { TreeLayout } from "../src/lib/hydra";
 
-test("hello", () => {
-  expect(hello("foo")).toEqual("Hello foo");
+test("tree layout", () => {
+  let root = new Tree([0, 0], []);
+  root.appendChild([-0.5, 1]);
+  let child = root.appendChild([0.5, 1]);
+  child.appendChild([0, 2]);
+  child.appendChild([1, 2]);
+
+  let layout = TreeLayout.fromTree(root);
+  layout.tree.zip(root).forEachPreorder(([p, t]) => {
+    expect(p.x).toEqual(t[0]);
+    expect(p.y).toEqual(t[1]);
+  });
 });
